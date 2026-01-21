c4e8c36458d9db82ba728b4ecf402856a71faba0
MohamedAit-oufquir <154386342+MohamedAit-oufquir@users.noreply.github.com>
2026-01-20 23:45:01 -0500

    First commit

---
+++ ecommerce-website/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
